3e5ca7375acfcc7f467f118f30f117c69e7a9db2
Sahar Sheikholeslami <sahara_sheik@hotmail.com>
2021-07-08 10:02:18 -0400

    Initial commit

---
+++ src/main/java/ucf/assignments/DisplayAllController.java
@@ -0,0 +1,21 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  Copyright 2021 Sahar Sheikholeslami
+ */
+
+package ucf.assignments;
+
+import javafx.event.ActionEvent;
+
+public class DisplayAllController {
+    public void loadCompleteList(ActionEvent actionEvent) {
+    }
+    // load all availble items within a list
+
+    // if (localStorage.getItem(ToDO) == NULL -> then print no data available
+    // else get the ToDos from JSON.Parse.LocalStorage.getitem(ToDo)
+    // afterwards ToDo.load (todo)
+
+
+
+}
